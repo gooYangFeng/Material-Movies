b16206cef7c73e21f147a0901ce18e6f5497e01e
yangfeng <e13310@gmail.com>
2015-07-30 13:58:07 +0800

    yf: change gradle build script to make test build happy.

--- HackVG/app/src/androidTest/java/com.hackvg.android.test/views.activities/MoviesActivityTest.java
+++ HackVG/app/src/androidTest/java/com.hackvg.android.test/views.activities/MoviesActivityTest.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.hackvg.android.test.views.activity;
+package com.hackvg.android.test.views.activities;
 
 import android.support.test.espresso.Espresso;
 import android.support.test.espresso.action.ViewActions;
